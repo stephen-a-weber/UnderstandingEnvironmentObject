6f6c41d9d57429268e95d79bcce5f4c3e23dfd81
stephen-a-weber <pecuniary@gmail.com>
2022-04-08 12:20:23 -0400

    Initial Commit

---
+++ UnderstandingEnvironmentObject/UnderstandingEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingEnvironmentObjectApp.swift
+//  UnderstandingEnvironmentObject
+//
+//  Created by stephen weber on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
